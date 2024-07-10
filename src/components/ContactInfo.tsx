b229ed20378648a37319134c9b6eadf662b481ca
sergeyreshetnyakov <sergey.reshetnyakov@icloud.com>
2024-07-10 21:34:05 +0300

    text display fixed

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -12,7 +12,7 @@ export default function ContactInfo({ children, href, contact }: Props) {
          <div className="w-16 py-2">
             <div className="flex justify-center">{children}</div>
          </div>
-         <div className="flex py-2 px-8 flex-row w-52 border-l border-black border-dashed">
+         <div className="flex py-2 px-8 flex-row w-52 md:text-sm border-l border-black border-dashed">
             <Link href={href} className="italic text-clip">
                {contact}
             </Link>
